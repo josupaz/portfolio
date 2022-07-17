ea5eaccb38daa1754a79fa4ff02ec8a1a9876617
unknown <jpaz@xytech.com.ar>
2022-07-17 11:33:09 -0300

    change languaje recaptcha

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -184,7 +184,7 @@ const Formulario = () => {
                 />
               </div>
               <div className="recaptcha">
-                <ReCAPTCHA ref={captcha} sitekey={SITE_KEY} onChange={verifyCallback} />
+                <ReCAPTCHA ref={captcha} sitekey={SITE_KEY} hl='en' onChange={verifyCallback} />
               </div>
               <br />
               <button type="submit">Send</button>
